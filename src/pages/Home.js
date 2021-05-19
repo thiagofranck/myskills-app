a170a243abc86518038e2b995594eed85a39e3f8
Thiago Franck <thiagohenrique776@gmail.com>
2021-05-19 14:20:11 -0300

    Estrutura JSX e componentes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { 
   View, 
   Text, 
@@ -10,6 +10,13 @@ import {
 
 
 export function Home(){
+  const [ newSkill, setNewSkill ] = useState('');
+  const [ mySkills, setMySkills ] = useState([]);
+
+  function handleAddSkill() {
+    setMySkills(oldState => [...oldState, newSkill]);
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>
@@ -20,20 +27,30 @@ export function Home(){
         style={styles.input}
         placeholder="New skill"
         placeholderTextColor="#555"
+        onChangeText={setNewSkill}
       />
 
       <TouchableOpacity 
         style={styles.button}
         activeOpacity={.7}
+        onPress={handleAddSkill}
       >
         <Text style={styles.buttonText}>Add</Text>
       </TouchableOpacity>
 
-      <Text style={[styles.title, {marginTop: 50}]}>
+      <Text style={[styles.title, {marginVertical: 50}]}>
         My Skills
       </Text>
 
-
+      {
+        mySkills.map(skill => (
+        <TouchableOpacity key={skill} style={styles.buttonSkill}>
+          <Text style={styles.textSkill}>
+            {skill}
+          </Text>
+        </TouchableOpacity>
+        ))
+      }  
     </View>
   )
 }
@@ -71,5 +88,17 @@ const styles = StyleSheet.create({
     color: '#FFF',
     fontSize: 17,
     fontWeight: 'bold'
-  }
+  },
+  buttonSkill: {
+    backgroundColor: '#1F1E25',
+    padding: 15,
+    borderRadius: 50,
+    alignItems: 'center',
+    marginVertical: 10
+  },
+  textSkill: {
+    color: '#FFF',
+    fontSize: 22,
+    fontWeight: 'bold',
+  },
 });
